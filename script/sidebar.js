55ff996dc6d227fb219a6a09c11db881797743b7
Dena Chen <49086173+cce932@users.noreply.github.com>
2021-09-30 23:11:37 +0800

    fix sub-menu and add open menu icon (#1)
    * fix: sub-menu should not disappear when hovering sub-menu-background
    * feature: add item_menu-open icon and change the naming of icons

--- script/sidebar.js
+++ script/sidebar.js
@@ -2,10 +2,8 @@ for (let i=1; i<=$( ".menu-item" ).length; i++) {
   $(`li[aria-owns='menu-item-${i}'], #menu-item-${i}`).hover(
     function() {
       $(`#menu-item-${i}`).show();
-      $("#sub-menu-background").show();
     }, function() {
       $(`#menu-item-${i}`).hide();
-      $("#sub-menu-background").hide();
     }
   );
 }
